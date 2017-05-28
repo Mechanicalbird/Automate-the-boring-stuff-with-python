53c7e095f807353a5361234ce9425063af38f82f
Mechanicalbird <mechanicalbird_just@yahoo.com>
2017-05-29 01:23:44 +0900

    Create page279-1.py

---
+++ page279-1.py
@@ -0,0 +1,17 @@
+import requests, os, bs4
+
+url = 'http://xkcd.com'
+os.makedirs('xkcd', exist_ok=True)
+
+while not url.endwith('#'):
+    # download the image
+
+    # find the URL of the image
+
+    # save the image
+
+    # to Prev button
+
+
+
+print('Done.')
